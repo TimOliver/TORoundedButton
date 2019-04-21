e585e2b2715f9445870c9b84ee7c3b77a1b9d9ae
Tim Oliver <me@timoliver.com.au>
2019-04-21 22:19:28 +0900

    Added button to IB

--- TORoundedButtonExample/ViewController.m
+++ TORoundedButtonExample/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "TORoundedButton.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
 
@@ -16,7 +19,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+
 }
 
 
