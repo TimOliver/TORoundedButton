ffe503212d204f13a7085da42df0addb8c643d12
Tim Oliver <me@timoliver.com.au>
2019-04-21 22:19:21 +0900

    Added button class

---
+++ TORoundedButton/TORoundedButton.h
@@ -0,0 +1,20 @@
+//
+//  TORoundedButton.h
+//  TORoundedButtonExample
+//
+//  Created by Tim Oliver on 21/4/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+IB_DESIGNABLE
+@interface TORoundedButton : UIButton
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TORoundedButton/TORoundedButton.m
@@ -0,0 +1,15 @@
+//
+//  TORoundedButton.m
+//  TORoundedButtonExample
+//
+//  Created by Tim Oliver on 21/4/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import "TORoundedButton.h"
+
+@implementation TORoundedButton
+
+
+
+@end
