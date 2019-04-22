895935c7852ca2abb987700ebe31722d87913000
Tim Oliver <me@timoliver.com.au>
2019-04-23 01:33:30 +0900

    Started adding public APIs to the header

--- TORoundedButton/TORoundedButton.h
+++ TORoundedButton/TORoundedButton.h
@@ -13,8 +13,19 @@ NS_ASSUME_NONNULL_BEGIN
 IB_DESIGNABLE
 @interface TORoundedButton : UIControl
 
+/** The text that is displayed in center of the button (Default is "Button") */
+@property (nonatomic, copy) IBInspectable NSString *text;
+
+/** The color of the text in this button (Default is white) */
+@property (nonatomic, strong) IBInspectable UIColor *textColor;
+
+/** The font of the text in the button (Default is size 21 Bold) */
+@property (nonatomic, strong) UIFont *textFont;
+
+/** The radius of the corners of this button (Default is 10.0f) */
 @property (nonatomic, assign) IBInspectable CGFloat cornerRadius;
 
+/** A callback handler triggered each time the button. */
 @property (nonatomic, copy) void (^tappedHandler)(void);
 
 @end
