2f0d84ceb61f33e000e4be4617f52b404497901f
Tim Oliver <me@timoliver.com.au>
2019-04-28 16:01:18 +0900

    Added two render paths for opaque and transparent buttons

--- TORoundedButton/TORoundedButton.m
+++ TORoundedButton/TORoundedButton.m
@@ -10,14 +10,39 @@
 
 @interface TORoundedButton ()
 
+/** Marked whenever the graphical components of the button need to be recalculated */
+@property (nonatomic, assign) BOOL isDirty;
+
+/** The title label displaying the text in the center of the button */
 @property (nonatomic, strong) UILabel *titleLabel;
+
+/** An image view that displays the rounded box behind the button text */
 @property (nonatomic, strong) UIImageView *backgroundImageView;
+
+/** The dynamically generated rounded box image that is applied to the image view */
+@property (nonatomic, strong, nullable) UIImage *backgroundImage;
+
+/** A resizable image that optionally shows when the button is tapped */
+@property (nonatomic, strong, nullable) UIImage *tappedBackgroundImage;
+
+/** Because this view is always clear, intercept and store the intended background color */
+@property (nonatomic, strong, nullable) UIColor *buttonBackgroundColor;
 
 @end
 
 @implementation TORoundedButton
 
 #pragma mark - View Creation -
+
+- (instancetype)initWithText:(NSString *)text
+{
+    if (self = [super initWithFrame:(CGRect){0,0, 288.0f, 50.0f}]) {
+        [self roundedButtonCommonInit];
+        _titleLabel.text = text;
+    }
+
+    return self;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
@@ -39,14 +64,20 @@
 
 - (void)roundedButtonCommonInit
 {
-    // Default properties
+    // Default properties (Make sure they're not overriding IB)
     _cornerRadius = 10.0f;
+    _tappedTextAlpha = 0.5f;
+    _tapAnimationDuration = 0.4f;
+    _isDirty = YES;
     self.opaque = YES;
+
+    if (!_buttonBackgroundColor) { _buttonBackgroundColor = [UIColor whiteColor]; }
+    super.backgroundColor = [UIColor clearColor];
 
     // Create sub views
     self.backgroundImageView = [[UIImageView alloc] initWithFrame:self.bounds];
     self.backgroundImageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    self.backgroundImageView.opaque = self.opaque; // Match self
+    self.backgroundImageView.opaque = self.opaque;
     self.backgroundImageView.clipsToBounds = YES;
     [self addSubview:self.backgroundImageView];
 
@@ -57,10 +88,7 @@
     self.titleLabel.backgroundColor = self.tintColor;
     [self addSubview:self.titleLabel];
 
-    self.titleLabel.text = @"Hello Twitch!";
-
-    UIImage *image = [[self class] buttonImageWithBackgroundColor:self.backgroundColor foregroundColor:self.tintColor cornerRadius:self.cornerRadius];
-    self.backgroundImageView.image = image;
+    self.titleLabel.text = @"Button";
 
     [self addTarget:self action:@selector(didTouchDownInside) forControlEvents:UIControlEventTouchDown];
     [self addTarget:self action:@selector(didTouchUpInside) forControlEvents:UIControlEventTouchUpInside];
@@ -68,22 +96,67 @@
     [self addTarget:self action:@selector(didDragInside) forControlEvents:UIControlEventTouchDragEnter];
 }
 
-#pragma mark - View Layout -
+#pragma mark - View Displaying -
 
 - (void)layoutSubviews
 {
     [super layoutSubviews];
+
+    if (self.isDirty) {
+        [self prepareViewsForDisplay];
+        self.isDirty = NO;
+    }
 
     [self.titleLabel sizeToFit];
     self.titleLabel.center = self.backgroundImageView.center;
     self.titleLabel.frame = CGRectIntegral(self.titleLabel.frame);
 }
 
+- (void)prepareViewsForDisplay
+{
+    if (self.opaque) {
+        [self prepareForOpaqueDisplay];
+    }
+    else {
+        [self prepareForTransparentDisplay];
+    }
+}
+
+- (void)prepareForOpaqueDisplay
+{
+    // Generate any images we need
+    self.backgroundImage = [[self class] buttonImageWithBackgroundColor:self.buttonBackgroundColor
+                                                        foregroundColor:self.tintColor
+                                                           cornerRadius:self.cornerRadius];
+
+    // Configure the background image view for opaque drawing
+    self.backgroundImageView.image = self.backgroundImage;
+    self.backgroundImageView.backgroundColor = nil;
+
+    // Reset ourselves from potential clipping
+    self.layer.masksToBounds = NO;
+    self.layer.cornerRadius = 0.0f;
+}
+
+- (void)prepareForTransparentDisplay
+{
+    // Clear out any opaque graphics
+    self.backgroundImage = nil;
+    self.tappedBackgroundImage = nil;
+
+    // Configure the background image view for transparent drawing
+    self.backgroundImageView.backgroundColor = self.tintColor;
+
+    // Configure ourselves for clipping the views
+    self.layer.masksToBounds = YES;
+    self.layer.cornerRadius = self.cornerRadius;
+}
+
 #pragma mark - Interaction -
 
 - (void)didTouchDownInside
 {
-    [self setLabelAlpha:0.5f animated:NO];
+    [self setLabelAlpha:self.tappedTextAlpha animated:NO];
 }
 
 - (void)didTouchUpInside
@@ -100,7 +173,7 @@
 
 - (void)didDragInside
 {
-    [self setLabelAlpha:0.5f animated:YES];
+    [self setLabelAlpha:self.tappedTextAlpha animated:YES];
 }
 
 - (void)setLabelAlpha:(CGFloat)labelAlpha animated:(BOOL)animated
@@ -114,13 +187,60 @@
         self.titleLabel.alpha = labelAlpha;
     };
 
-    [UIView animateWithDuration:0.4f
+    [UIView animateWithDuration:self.tapAnimationDuration
                           delay:0.0f
                         options:UIViewAnimationOptionBeginFromCurrentState
                      animations:animationBlock completion:nil];
 }
 
-#pragma mark - Graphics Rendering -
+#pragma mark - Public Accessors -
+
+/** Map the text property directly to the label */
+- (void)setText:(NSString *)text { self.titleLabel.text = text; }
+- (NSString *)text { return self.titleLabel.text; }
+
+/** Map the font property directly to the label */
+- (void)setTextFont:(UIFont *)textFont
+{
+    self.titleLabel.font = textFont;
+    self.textPointSize = 0.0f; // Reset the IB text point size back to disabled
+}
+- (UIFont *)textFont { return self.titleLabel.font; }
+
+- (void)setTextPointSize:(CGFloat)textPointSize
+{
+    if (_textPointSize == textPointSize) { return; }
+    _textPointSize = textPointSize;
+    self.titleLabel.font = [UIFont boldSystemFontOfSize:textPointSize];
+}
+
+- (void)setBackgroundColor:(UIColor *)backgroundColor
+{
+    if (backgroundColor == _buttonBackgroundColor) { return; }
+    _buttonBackgroundColor = backgroundColor;
+    _isDirty = YES;
+    [self setNeedsLayout];
+}
+
+- (BOOL)isOpaque
+{
+    return [[self class] isOpaqueColor:self.buttonBackgroundColor];
+}
+
+- (void)setOpaque:(BOOL)opaque
+{
+    [super setOpaque:[[self class] isOpaqueColor:self.buttonBackgroundColor]];
+}
+
+#pragma mark - Graphics Handling -
++ (BOOL)isOpaqueColor:(UIColor *)color
+{
+    // If the background color's _alpha_ is anything other than 1.0, it's not opaque. ;)
+    CGFloat a = 0;
+    [color getRed:NULL green:NULL blue:NULL alpha:&a];
+    return a >= (1.0f - FLT_EPSILON);
+}
+
 + (UIImage *)buttonImageWithBackgroundColor:(UIColor *)backgroundColor
                             foregroundColor:(UIColor *)foregroundColor
                                cornerRadius:(CGFloat)cornerRadius
@@ -128,13 +248,9 @@
 
     CGFloat dimensionSize = (cornerRadius * 2.0f) + 2.0f;
     CGSize size = (CGSize){dimensionSize, dimensionSize};
-    CGFloat r = 0, g = 0, b = 0, a = 0;
+
     UIGraphicsImageRendererFormat *format = [[UIGraphicsImageRendererFormat alloc] init];
-
-    // If the background color's _alpha_ is anything other than 1.0, it's not opaque ;)
-    [backgroundColor getRed: &r green: &g blue: &b alpha: &a];
-
-    format.opaque = a == 1.0;
+    format.opaque = [[self class] isOpaqueColor:backgroundColor];
 
     UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:size format:format];
     UIImage *image = [renderer imageWithActions:^(UIGraphicsImageRendererContext *rendererContext) {
