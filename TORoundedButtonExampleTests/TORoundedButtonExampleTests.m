995643b28ef9f7e815d29dbb6827327fe1cafb09
Tim Oliver <me@timoliver.com.au>
2019-04-30 18:48:27 +0900

    Added initial unit test

--- TORoundedButtonExampleTests/TORoundedButtonExampleTests.m
+++ TORoundedButtonExampleTests/TORoundedButtonExampleTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "TORoundedButton.h"
 
 @interface TORoundedButtonExampleTests : XCTestCase
 
@@ -14,24 +15,11 @@
 
 @implementation TORoundedButtonExampleTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+- (void)testDefaultValues
+{
+    TORoundedButton *button = [[TORoundedButton alloc] initWithText:@"Test"];
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    XCTAssertNotNil(button);
 }
 
 @end
