1202b6ae188a0f9c6543241a89912cc017ea9a5b
Simone Gasparini <simone.gasparini@dualb.it>
2019-10-29 15:17:55 +0100

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TORoundedButton",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         .library(
             name: "TORoundedButton",
@@ -14,6 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "TORoundedButton",
+            path: "TORoundedButton",
             exclude: [
                 "TORoundedButtonExample",
                 "TORoundedButtonExampleTest",
