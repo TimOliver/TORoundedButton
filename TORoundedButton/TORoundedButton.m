68ebc5f4f300b9dd7fe67fb0560f1a18dd38b9bc
Tim Oliver <git@tim.dev>
2023-10-08 10:34:49 +0900

    Added comment for direct methods

--- TORoundedButton/TORoundedButton.m
+++ TORoundedButton/TORoundedButton.m
@@ -20,6 +20,7 @@
 //  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
 //  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+// Objective-C direct methods - https://nshipster.com/direct/
 #define TOROUNDEDBUTTON_OBJC_DIRECT __attribute__((objc_direct))
 
 #import "TORoundedButton.h"
